23589684090d9951a7111d379a1966c3c5196a8b
justindileonardo <44879661+justindileonardo@users.noreply.github.com>
2019-11-14 15:08:47 -0500

    some stuff

--- JD_FlagsOfTheWorldGame/Assets/Scripts/Submission.cs
+++ JD_FlagsOfTheWorldGame/Assets/Scripts/Submission.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Submission : MonoBehaviour
 {
     private Selection selectionScript;
     bool correct;
+    public Text text_countryNamePopup;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,17 +18,24 @@ public class Submission : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(text_countryNamePopup.color.a > 0)
+        {
+            text_countryNamePopup.color = new Color(0, 0, 0, text_countryNamePopup.color.a - 0.005f);
+        }
+        
         
     }
-
+    //when you click the button of a flag
     public void MakeSubmission(GameObject correctCountry)
     {
+        //if you select right country's flag...
         if(selectionScript.currentGeography == correctCountry)
         {
             print("correct!");
             correct = true;
             selectionScript.Deselect();
         }
+        //if you select wrong country's flag...
         else
         {
             print("WRONG!!!");
@@ -35,9 +45,27 @@ public class Submission : MonoBehaviour
     }
     public void TurnColorOn(SpriteRenderer countrySpriteColor)
     {
+        //if you select right country's flag
         if(correct == true)
         {
+            //turn flag color on geography
             countrySpriteColor.enabled = true;
-        }        
+            //disable the flag button
+            EventSystem.current.currentSelectedGameObject.GetComponent<Button>().interactable = false;
+        }
+        
     }
+
+    public void TextCountryName(string countryName)
+    {
+        if(correct == true)
+        {
+            text_countryNamePopup.enabled = true;
+            text_countryNamePopup.text = countryName;
+            text_countryNamePopup.color = new Color(0, 0, 0, 1);
+        }
+        
+    }
+
+    
 }
